b319c061e30cd66765f41f8bda261ec29fad4de5
Junpei Ihara <junpei.ihara@quoine.com>
2022-03-13 23:47:44 +0900

    Express の導入

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
-const functions = require("firebase-functions");
+// index.js
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const functions = require("firebase-functions");
+// Expressの読み込み
+const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  // レスポンスの設定
+  res.send("Hello Express!");
 });
+
+// 出力
+const api = functions.https.onRequest(app);
+module.exports = { api };
