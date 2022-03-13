d1283e6253ed88646ec61563219618f38523daa8
Junpei Ihara <junpei.ihara@quoine.com>
2022-03-14 00:04:32 +0900

    CORS 対策

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,11 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const requestPromise = require("request-promise-native"); // 追加
+const cors = require("cors"); // 追加
 
 const app = express();
+
+app.use(cors()); // 追加
 
 // APIにリクエストを送る関数を定義
 const getDataFromApi = async (keyword) => {
@@ -19,7 +22,18 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/user/:userId", (req, res) => {
-  // 省略
+  const users = [
+    { id: 1, name: "ジョナサン" },
+    { id: 2, name: "ジョセフ" },
+    { id: 3, name: "承太郎" },
+    { id: 4, name: "仗助" },
+    { id: 5, name: "ジョルノ" },
+  ];
+  // req.params.userIdでURLの後ろにつけた値をとれる．
+  const targetUser = users.find(
+    (user) => user.id === Number(req.params.userId)
+  );
+  res.send(targetUser);
 });
 
 // エンドポイント追加
