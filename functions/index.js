095fd461503bfeaf193740bd6656339702542807
Junpei Ihara <junpei.ihara@quoine.com>
2022-03-13 23:51:51 +0900

    URL のパラメータ取得

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,28 @@
-// index.js
-
 const functions = require("firebase-functions");
-// Expressの読み込み
 const express = require("express");
 
 const app = express();
 
 app.get("/hello", (req, res) => {
-  // レスポンスの設定
   res.send("Hello Express!");
 });
 
-// 出力
+// ↓↓↓ エンドポイントを追加 ↓↓↓
+app.get("/user/:userId", (req, res) => {
+  const users = [
+    { id: 1, name: "ジョナサン" },
+    { id: 2, name: "ジョセフ" },
+    { id: 3, name: "承太郎" },
+    { id: 4, name: "仗助" },
+    { id: 5, name: "ジョルノ" },
+  ];
+  // req.params.userIdでURLの後ろにつけた値をとれる．
+  const targetUser = users.find(
+    (user) => user.id === Number(req.params.userId)
+  );
+  res.send(targetUser);
+});
+
+// 以降変更なし
 const api = functions.https.onRequest(app);
 module.exports = { api };
